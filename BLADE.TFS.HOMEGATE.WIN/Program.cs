cc176c5f21082dae3aa44d5ce1238955f839afa4
blade fengye <fengyeblade@hotmail.com>
2026-06-22 10:51:30 +0800

    a

--- BLADE.TFS.HOMEGATE.WIN/Program.cs
+++ BLADE.TFS.HOMEGATE.WIN/Program.cs
@@ -3,6 +3,6 @@ using BLADE.TFS.HOMEGATE.WIN;
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddWindowsService();
 builder.Services.AddHostedService<HomeGateWinWorker>();
-
+Directory.SetCurrentDirectory(AppContext.BaseDirectory);
 var host = builder.Build();
 host.Run();
